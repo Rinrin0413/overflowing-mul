c3b331a5fdbae750bbb41b50eceb36b4afdb3ddc
Rinrin.rs <rinrin0413.valley@gmail.com>
2023-06-14 13:39:35 +0900

    🐸 Change: some element names

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use rand::Rng;
 use std::{fmt, io};
 
-struct MulFormula {
+struct Problem {
     multiplicand: u16,
     multiplier: u16,
 }
 
-impl MulFormula {
+impl Problem {
     fn new(multiplicand: u16, multiplier: u16) -> Self {
         Self {
             multiplicand,
             multiplier,
         }
     }
-    fn result(&self) -> u32 {
+    fn answer(&self) -> u32 {
         self.multiplicand as u32 * self.multiplier as u32
     }
 }
 
-impl fmt::Display for MulFormula {
+impl fmt::Display for Problem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{} × {} =", self.multiplicand, self.multiplier)
     }
@@ -27,10 +27,10 @@ impl fmt::Display for MulFormula {
 fn main() {
     let mut rng = rand::thread_rng();
     loop {
-        let q = MulFormula::new(rng.gen_range(10..999), rng.gen_range(1..99));
-        let q_fmt = q.to_string();
-        let q_rslt = q.result();
-        println!("解を答えてください:\n{}", q_fmt);
+        let problem = Problem::new(rng.gen_range(10..999), rng.gen_range(1..99));
+        let formula = problem.to_string();
+        let answer = problem.answer();
+        println!("{}", formula);
         loop {
             let mut input = String::new();
             io::stdin().read_line(&mut input).expect("読み取りに失敗");
@@ -44,11 +44,11 @@ fn main() {
             if input < 0 {
                 println!("正の数を入力してください:");
                 continue;
-            } else if q_rslt != input as u32 {
-                println!("不正解:\n{} の解は {} ではありません。", q_fmt, input.abs());
+            } else if answer != input as u32 {
+                println!("不正解:\n{} の解は {} ではありません。", formula, input.abs());
                 continue;
             } else {
-                println!("正解: {} {}", q_fmt, q_rslt);
+                println!("正解: {} {}", formula, answer);
                 println!("================================");
                 println!("次の問題");
                 break;
